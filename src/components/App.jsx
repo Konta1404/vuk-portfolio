dd3c32396941e037e6900f35c34294a590805435
Veselin <veselin.kontic@synergysuite.com>
2022-02-15 22:58:09 +0100

    Fixes react import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import UnderConstruction from "./UnderConstruction/UnderConstruction";
 import './UnderConstruction/under-construction.css';
 

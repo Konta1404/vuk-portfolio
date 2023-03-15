a7f58d02df1ed7b07ae3ff16920bc81aafc91d34
Veselin <veselin.kontic@synergysuite.com>
2023-03-15 11:33:24 +0100

    Changes font for title

--- src/components/UnderConstruction/UnderConstruction.jsx
+++ src/components/UnderConstruction/UnderConstruction.jsx
@@ -5,7 +5,7 @@ const UnderConstruction = () => {
         <div className="mt-8">
             <div className="grid px-4 md:px-none md:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
                 <p className="text-white font-description font-sans text-base lg:text-xl opacity-60">Website is under construction. It’s gonna take some time, but in mean while feel free to drop me an <a href="mailto:vukgraphicart@gmail.com" className="underline">email</a> if you have any questions.</p>
-                <p className="text-white font-description font-sans text-base lg:text-xl">Digital, Branding, Brand Strategy, Concept Development, Natural Interfaces, Art Direction, Web, Mobile, Digital Products. Co-founder and Head of Design of Happy Wolf Studio. Art director and Co-founder of <a href="https://www.wind-fest.me" className="underline" target="_blank" rel="noreferrer">Wind Fest</a>.</p>
+                <p className="text-white font-description font-sans text-base lg:text-xl">Digital, Branding, Brand Strategy, Concept Development, Natural Interfaces, Art Direction, Web, Mobile, Digital Products. Co-founder and Head of Design of <a href="https://happywolf.me" className="underline" target="_blank" rel="noreferrer">Happy Wolf Studio</a>. Art director and Co-founder of <a href="https://www.wind-fest.me" className="underline" target="_blank" rel="noreferrer">Wind Fest</a>.</p>
             </div>
             <div className="px-4 md:px-none">
                 <h1 className="text-3xl md: text-5xl lg:text-8xl font-title font-bold text-white">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       }
     },
     fontFamily: {
-      title: ['Woodland'],
+      title: ['Zodiak-Regular'],
       description: ['NeueMontreal']
     }
   },
